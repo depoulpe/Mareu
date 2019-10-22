3ef0bbe0c27ffb1410aa8c845f51ff93aa041c28
depoulpe <depoulpe@gmail.com>
2019-10-22 21:32:42 +0200

    implement floating action button

--- app/src/main/java/com/example/mareu/MainActivity.java
+++ app/src/main/java/com/example/mareu/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mareu;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             //   Intent meetingActivity = new Intent(view.getContext(), MeetingActivity.class);
+
+             //   startActivity(meetingActivity);
+                //  Snackbar.make(view, "Here's a Snackbar Notification", Snackbar.LENGTH_LONG)
+                //    .setAction("Action", null).show();
+            }
+
+        });
     }
 }
